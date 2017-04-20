61d4c61464c2dec377b5e917d3f237e2afc00dfd
ViWu <viwu@ucdavis.edu>
2017-04-20 16:17:05 -0700

    Added splash screen
    Added a splash screen that appears for a few seconds when launching the application. Includes the icon, title, and some other texts. Makes for a more professional presentation.

---
+++ app/src/main/java/myapplication/flashcards/Splash.java
@@ -0,0 +1,31 @@
+package myapplication.flashcards;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    private final static int SPLASH_DURATION= 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(SPLASH_DURATION);
+                    Intent intent = new Intent(getApplicationContext(),MainMenu.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
